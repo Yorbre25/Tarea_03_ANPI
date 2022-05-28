bd4433f6158e76cccb3a1e8cb3cc4fbfbe064dfa
Yorbre25 <yraulbr25@gmail.com>
2022-05-28 11:49:51 -0600

    add Cuadratura Gaussiana, Trapecio y Simpson

---
+++ Metodos/CuadraturasGaussianas.py
@@ -0,0 +1,83 @@
+import numpy
+import sympy as sp
+import numpy as np
+import math
+
+"""
+Metodo cuadratrura gaussiana: Integración numérica.
+"""
+
+
+"""
+Cuadratura Gaussiana General: Antes de llamar la función cuadratura gaussiana
+covierte la función de entrada para que calce en el intervalo [-1,1]
+    Entrada:
+        f: Función 
+        a: límite inferior
+        b: límite superior
+        n: grado del polinomio de Legendre
+    Salida:
+        Integral de la función
+"""
+def cuadratura_gaussiana_general(f, a, b, n):
+    x = sp.Symbol("x")
+    f = sp.simplify(f)
+    g = (b-a)/2 * f.subs(x,((b-a)*x+b+a)/2)
+    # print(g)
+    return cuadratura_gaussiana(g, n)
+
+
+
+"""
+Cuadraturas Gaussianas: Se calcula la integral de una función,
+utilizando un polinomio de Legendre. La integral debe ser desde el 
+intervalo -1 a 1
+Entrada:
+    f: Función 
+    n: grado del polinomio de Legendre
+Salida:
+    Integral de la función
+"""
+def cuadratura_gaussiana(f, n):
+    #Ceros del polinomio
+    resul = ceros_pol_legendre(n)
+    xv = resul[0]
+    xv = np.array(xv)
+    p = resul[1]
+    #Calcular las w
+    w = get_w(xv, p)
+
+    x = sp.Symbol("x")
+    f = sp.sympify(f)
+    I = 0
+    #Calcular la integral (I = w0*f(x0) + w1*f(x1)+...)
+    for i in range(n):
+        I += w[i]*f.subs(x,xv[i]).evalf()
+    return I
+
+
+def ceros_pol_legendre(n):
+    x = sp.Symbol("x")
+    q = sp.diff((x**2 - 1)**n, x, n)
+    print(q)
+    p = (1/(math.factorial(n) * 2**n ) * q)
+    xv = sp.solve(p)
+    return xv,p
+
+def get_w(xv, p):
+    x = sp.Symbol("x")
+    n = xv.size
+    pd = sp.diff(p, x)
+    pf_n = np.zeros(n)
+    for i in range(n):
+        pf_n[i] = pd.subs(x,xv[i]).evalf()
+    w = 2/((1-xv**2)*(pf_n**2))
+    return w
+
+
+
+f = "exp(x) * cos(x)"
+n = 3
+# I = cuadratura_gaussiana(f, n)
+I = cuadratura_gaussiana_general(f, 2, 4, n)
+print(I)
---
+++ Metodos/Trapecio y Simpson.py
@@ -0,0 +1,65 @@
+from re import I
+import sympy as sp
+
+"""
+* INTEGRACIÓN NUMÉRICA
+"""
+"""
+Metodo del trapecio: Utiliza un polinomio de grado 1
+para aproximar el area debajo de la curva
+"""
+def trapecio(f, a, b):
+    x = sp.Symbol("x")
+    f = sp.sympify(f)
+    f_a = f.subs(x, a).evalf()
+    f_b = f.subs(x, b).evalf()
+    I = ((f_a + f_b))*(b - a)/2
+    return I
+"""
+Metodo de Simpson: Utiliza un polinomio de grado 2 
+para aproximar el area bajo la curva
+"""
+def simpson(f, a, b):
+    x = sp.Symbol("x")
+    f = sp.sympify(f)
+    f_a = f.subs(x, a).evalf()
+    f_b = f.subs(x, b).evalf()
+    f_medio = f.subs(x, (a + b)/2).evalf()
+    I = ((b - a)/6)    *(f_a + 4*f_medio + f_b)
+    return I
+
+def trapecio_compuesto(f, a, b, m):
+    x = sp.Symbol("x")
+    f = sp.sympify(f)    
+    h = (b - a)/(m - 1)
+    preima = []
+    for i in range(m):
+        preima.append(a + i*h)
+    
+    I = 0
+    for j in range(len(preima) - 1):
+        I += trapecio(f, preima[j], preima[j + 1])
+    # print(I)
+    return I
+
+def simpson_compuesto(f, a, b, m):
+    x = sp.Symbol("x")
+    f = sp.sympify(f)    
+    h = (b - a)/(m - 1)
+    preima = []
+    for i in range(m):
+        preima.append(a + i*h)
+    
+    I = 0
+    for j in range(len(preima) - 1):
+        I += simpson(f, preima[j], preima[j + 1])
+    # print(I)
+    return I
+
+f = "exp(x)"
+# X = trapecio(f, 0, 1)
+# print(X)
+# X = simpson(f, 0, 1)
+# print(X)
+trapecio_compuesto(f, 0, 1, 5)
+simpson_compuesto(f, 0, 1, 5)
